4df156a7d98d99cabc199c7aaf37a9d140b40e92
imiller142 <64618300+imiller142@users.noreply.github.com>
2021-06-14 11:07:19 -0500

    Further defined classes

--- Core.py
+++ Core.py
@@ -1,10 +1,11 @@
 import random
 
+SUITS = ('Clubs', 'Diamonds', 'Spades', 'Heats')
+RANKS = ('Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King')
+VALUE = ['Ace': 1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'Jack':10, 'Queen':10, 'King':10]
+
+
 class Card:
-    
-    SUITS = ('Clubs', 'Diamonds', 'Spades', 'Heats')
-    RANKS = ('Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King')
-    VALUE = ['Ace': 1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'Jack':10, 'Queen':10, 'King':10
     
     def __init__(self, suit, rank):
         self.suit = suit
@@ -19,11 +20,17 @@ class Card:
 class Deck:
     
     def __init__(self)
-        pass
+        self.all_cards = []
+        for suit in SUITS:
+            for rank in RANKS
+                return self.all_cards.append(Card(suit, rank))
     
 class Player:
     
     def __init__(self, name, bank)
-        pass
-    
-    
+        self.name = name
+        self.bank = bank
+        
+
+class Hand(Deck):
+             
