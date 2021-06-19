fbb5efcdca24c9cdcaaf8ad45388f7fdb963cddb
Ian Miller <imiller142@gmail.com>
2021-06-19 07:41:19 -0500

    attempting to make progress

--- Core.py
+++ Core.py
@@ -2,7 +2,7 @@ import random
 
 SUITS = ('Clubs', 'Diamonds', 'Spades', 'Heats')
 RANKS = ('Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King')
-VALUE = ['Ace': 1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'Jack':10, 'Queen':10, 'King':10]
+VALUE = {'Ace': 1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'Jack':10, 'Queen':10, 'King':10}
 
 
 class Card:
@@ -13,24 +13,35 @@ class Card:
         
     # Will return value of card
     def __str__(self):
-        retrun f'{Card.RANKS[self.rank]} of {Card.SUITS[self.suit]}'
+        return self.rank + ' of ' + self.suit
         
 
 #deck will take cards
 class Deck:
     
-    def __init__(self)
-        self.all_cards = []
+    def __init__(self):
+        self.deck = []
         for suit in SUITS:
-            for rank in RANKS
-                return self.all_cards.append(Card(suit, rank))
+            for rank in RANKS:
+                return self.deck.append(Card(suit, rank))
+
+    def __str__(self):
+        pass
     
 class Player:
     
-    def __init__(self, name, bank)
+    def __init__(self,name):
         self.name = name
-        self.bank = bank
+
+    def bank(self):
+        pass
         
 
+    
 class Hand(Deck):
-             
+    pass
+
+playerOne = Player()
+
+playerOne.get_name
+
